d3739414a5842f52bfa35bc64f78618c138c4587
Moana-rose Marcello <mmarcell@f1r2s7.codam.nl>
2020-01-12 17:54:29 +0100

    add test for one_string_non_num_char_beg

--- tests/srcs/test_share_input_validation.c
+++ tests/srcs/test_share_input_validation.c
@@ -6,7 +6,7 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/12 17:10:41 by mmarcell       #+#    #+#                */
-/*   Updated: 2020/01/12 17:39:34 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/01/12 17:53:00 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ Test(sh_input_validation, one_string_non_num_char_beg)
 	t_clist	*stack_a;
 	char	**argv;
 
-	cr_assert(validate_input(1, &"", &stack_a) ==  , "");
-	cr_assert(validate_input(1, &"", &stack_a) ==  , "");
-	cr_assert(validate_input(1, &"", &stack_a) ==  , "");
+	cr_assert(validate_input(1, &"  3 6 2345", &stack_a) == 0 , "one string input beginning with space should be valid.");
+	cr_assert(validate_input(1, &"hdk4 222355 24 0 39", &stack_a) == 1 , "one string input beginning with non num charackter should be invalid.");
+	cr_assert(validate_input(1, &"\n 332245 3 5 6 3446 0", &stack_a) == 1 , "one string input beginning with non num charackter should be invalid.");
 }
 
 Test(sh_input_validation, one_string_non_num_char_end)
