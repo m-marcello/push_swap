7297913fb5036b499431e3fde30406a91714573d
Moana-rose Marcello <mmarcell@f0r2s7.codam.nl>
2020-01-14 17:43:04 +0100

    change static function names for better representation of their purpose

--- srcs/share_input_validation.c
+++ srcs/share_input_validation.c
@@ -6,7 +6,7 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/12 16:05:14 by mmarcell       #+#    #+#                */
-/*   Updated: 2020/01/13 19:10:49 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/01/14 17:41:35 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 /*
 ** -------------------------------------------------------------------------- **
-** using atoi to generate numbers from an array of strings and apppends them
+** using atoi to generate numbers from an array of strings to apppend them
 ** to stack a
+**
+** str_arr must only consist of numbers (and their sign)
 **
 ** params
 **	char **str_arr		one input string split into multiple strings at ' '
-**	t_clist **stack_a	pointer to first element of stack_a
+**	t_clist **head_a	pointer to first element of stack a
 ** return
 **	1					when everthing went fine
 **	0					in case of error
 */
 
-static int	append_data(char **str_arr, t_clist **stack_a)
+static int	array_atoi(char **str_arr, t_clist **head_a)
 {
 	int				i;
 	long long int	data;
@@ -38,7 +40,14 @@ static int	append_data(char **str_arr, t_clist **stack_a)
 		data = ft_atoi(str_arr[i]);
 		if (data < INT_MIN || INT_MAX < data)
 		{
-			free_stuff(str_arr, stack_a);
+			free_str_arr(str_arr);
+			free_stack(head_a);
+			return (0);
+		}
+		if (!append_to_stack(data, head_a))
+		{
+			free_str_arr(str_arr);
+			free_stack(head_a);
 			return (0);
 		}
 		i++;
@@ -48,26 +57,26 @@ static int	append_data(char **str_arr, t_clist **stack_a)
 
 /*
 ** -------------------------------------------------------------------------- **
-** splits the input strings at ' ' and generates a circular list representing
-** stack a
+** splits the input strings at ' ' and passes the string array to array_atoi
+** to make integers out of the input strings
 **
 ** params
 **	int argc			number of strings of argv
 **	char **argv			address of first argument of main
-**	t_clist **stack_a	pointer to first element of stack a
+**	t_clist **head_a	pointer to first element of stack a
 ** return
 **	1					when everthing went fine
 **	0					in case of error
 */
 
-static int	generate_stack_a(int argc, char **argv, t_clist **stack_a)
+static int	split_input_strings(int argc, char **argv, t_clist **head_a)
 {
 	int		i;
 
 	i = 0;
 	while (i < argc)
 	{
-		if (!append_data(ft_strsplit(argv[i], ' '), stack_a))
+		if (!array_atoi(ft_strsplit(argv[i], ' '), head_a))
 			return (0);
 		i++;
 	}
@@ -84,13 +93,13 @@ static int	generate_stack_a(int argc, char **argv, t_clist **stack_a)
 ** params
 **	int argc			number of strings of argv
 **	char **argv			address of first argument of main
-**	t_clist **stack_a	pointer to first element of stack a
+**	t_clist **head_a	pointer to first element of stack a
 ** return
 **	1					in case of valid input
 **	0					in case of invalid input
 */
 
-int			is_valid_input(int argc, char **argv, t_clist **stack_a)
+int			is_valid_input(int argc, char **argv, t_clist **head_a)
 {
 	int		i;
 	int		j;
@@ -109,7 +118,7 @@ int			is_valid_input(int argc, char **argv, t_clist **stack_a)
 		}
 		i++;
 	}
-	if (!generate_stack_a(stack_a, argv))
+	if (!split_input_strings(argc, argv, head_a))
 		return (0);
 	return (1);
 }
