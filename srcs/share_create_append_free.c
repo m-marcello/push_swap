7a239a52939ed77a4b0a8b9047c292b57e0799c1
Moana-rose Marcello <mmarcell@f1r2s7.codam.nl>
2020-01-18 15:31:55 +0100

    write create_append_free not tested yet

--- srcs/share_create_append_free.c
+++ srcs/share_create_append_free.c
@@ -6,7 +6,7 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/14 14:25:06 by mmarcell       #+#    #+#                */
-/*   Updated: 2020/01/16 20:02:39 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/01/18 15:30:25 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,100 @@
 
 /*
 ** -------------------------------------------------------------------------- **
-** frees each node of the stack an the struct in which it is saved
+** sets the values of one node of the stack to 0 and frees its pointer
+**
+** params
+**	t_stack **node	pointer to the node
+*/
+
+static void	free_node(t_clist **node)
+{
+	(*node)->next = 0;
+	(*node)->prev = 0;
+	(*node)->data = 0;
+	free(*node);
+	*node = 0;
+}
+
+/*
+** -------------------------------------------------------------------------- **
+** walks through the stack to save each node
 **
 ** params
 **	t_stack **stack	pointer to struct representing stack
 */
 
-void	free_stack(t_stack **stack)
+void		free_stack(t_stack **stack)
 {
-	t_stack	*walk;
+	t_clist	*walk;
 
-	walk = (*stack)->head;
+	if (*stack == 0)
+		return ;
+	walk = (*stack)->head->next;
+	(*stack)->head->prev->next = 0;
+	free_node(&((*stack)->head));
+	while (walk)
+	{
+		walk = walk->next;
+		free_node((walk->prev));
+	}
+	(*stack)->head = 0;
+	(*stack)->node_count = 0;
+	free(*stack);
+	*stack = 0;
 }
 
 /*
 ** -------------------------------------------------------------------------- **
-** frees an array of strings
+** frees an array of strings and sets everything to 0
 **
 ** params
 **	char	***arr	address of the string array
 */
 
-void	free_str_array(char ***str_arr)
+void		free_str_array(char ***str_arr)
 {
-	;
+	int		i;
+	int		j;
+
+	i = 0;
+	while ((*str_arr)[i])
+	{
+		j = 0;
+		while ((*str_arr)[i][j])
+		{
+			(*str_arr)[i][j] = 0;
+			j++;
+		}
+		free((*str_arr)[i]);
+		(*str_arr)[i] = 0;
+		i++;
+	}
+	free(**str_arr);
+	**str_arr = 0;
+}
+
+/*
+** -------------------------------------------------------------------------- **
+** creates one node of t_clist and initializes it
+**
+** params
+**	t_clist **node	addres to node that needs to be crated
+**	int data		integer item of the stack for this node
+** return
+**	1				when everything went fine
+**	0				in case of error
+*/
+
+static int	create_node(t_clist **node, int data)
+{
+	*node = malloc(sizeof(t_clist));
+	if (*node == 0)
+		return (0);
+	(*node)->data = data;
+	(*node)->next = (*node);
+	(*node)->prev = (*node);
+	return (1);
 }
 
 /*
@@ -53,17 +123,26 @@ void	free_str_array(char ***str_arr)
 **	0				in case of error
 */
 
-void	append_to_stack(int data, t_stack **stack)
+void		append_to_stack(int data, t_stack **stack)
 {
-	;
+	t_clist	*new_node;
+
+	if (create_node(&new_node, data) == 0)
+		return (0);
+	if ((*stack)->head == 0)
+		(*stack)->head = new_node;
+	else if ((*stack)->head->next == (*stack)->head->prev)
+	{
+		(*stack)->head->next = new_node;
+		(*stack)->head->prev = new_node;
+		new_node->next = (*stack)->head;
+		new_node->prev = (*stack)->head;
+	}
+	else
+	{
+		new_node->next = (*stack)->head;
+		new_node->prev = (*stack)->head->prev;
+		(*stack)->head->prev = new_node;
+	}
+	return (1);
 }
-
-/*
-** -------------------------------------------------------------------------- **
-**
-*/
-
-/*
-** -------------------------------------------------------------------------- **
-**
-*/
