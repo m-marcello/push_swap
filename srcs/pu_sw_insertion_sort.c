4e162316fd703a6f72579d6e799251796015bb8f
Moana-rose Marcello <mmarcell@f1r2s16.codam.nl>
2020-02-12 20:51:23 +0100

    create pre_sort file because insertion_sort got too big

--- srcs/pu_sw_insertion_sort.c
+++ srcs/pu_sw_insertion_sort.c
@@ -6,62 +6,13 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/30 22:08:08 by mmarcell       #+#    #+#                */
-/*   Updated: 2020/02/12 18:51:44 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/02/12 19:39:56 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pu_sw.h"
 #include "libft.h"
 #include <stdlib.h>
-
-static int	index_in_eps(t_clist *node, unsigned long eps,
-			unsigned long total_nodes)
-{
-	return (node->index < eps || node->index + eps > total_nodes);
-}
-
-/*
-** -------------------------------------------------------------------------- **
-** pushes elements from stack a to stack b until either there are only 3
-** elements remaining in stack a or stack a is sorted (smallest element does
-** not necessarily need to be the first)
-** after applying this function stack a will be sorted - stage 2
-** stack b might still contain elements
-**
-** params
-**	t_print *p_info		struct with all the relevant info regarding printing
-**	t_stack *stack_a	pointer to struct representing stack a
-**	t_stack *stack_b	pointer to struct representing stack b
-** return
-**	VOID
-*/
-
-void	pre_sort(t_print *p_info, t_stack *stack_a, t_stack *stack_b)
-{
-	unsigned long	eps;
-	unsigned long	total_nodes;
-
-	eps = 15;
-	total_nodes = stack_a->node_count;
-	while (is_sorted(stack_a) == 0 && stack_a->node_count > 3)
-	{
-		if (index_in_eps(stack_a->head, eps, total_nodes))
-			pb(p_info, stack_a, stack_b);
-		else if (index_in_eps(stack_a->head->prev, eps, total_nodes) &&
-			index_in_eps(stack_a->head->next, eps, total_nodes))
-			sa(p_info, stack_a, stack_b);
-		else if (index_in_eps(stack_a->head->prev, eps, total_nodes))
-			rra(p_info, stack_a, stack_b);
-		else
-			ra(p_info, stack_a, stack_b);
-		if (stack_b->head != 0 && stack_b->head->index * 2 < total_nodes)
-			rb(p_info, stack_a, stack_b);
-		if (stack_b->node_count == 2 * eps - 1)
-			eps += 15;
-	}
-	if (is_sorted(stack_a) == 0 && stack_a->node_count == 3)
-		sa(p_info, stack_a, stack_b);
-}
 
 /*
 ** -------------------------------------------------------------------------- **
